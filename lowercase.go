08987e0abfe69e2f6f70920ed36fd3dde2d8605c
Dionisio Perez-Mavrogenis <dio@trinnylondon.com>
2020-12-01 16:36:51 +0000

    added tests

--- lowercase.go
+++ lowercase.go
@@ -1,40 +1,36 @@
 package lowercase
 
 import (
-	"bytes"
 	"context"
-	"fmt"
-	"strings"
-	"regexp"
 	"net/http"
-	"text/template"
+	"regexp"
+	"strings"
 )
+
+var uppercaseChars = regexp.MustCompile(`.*[A-Z].*`)
 
 type Config struct {
 }
 
-
 func CreateConfig() *Config {
-	return &Config{
-	}
+	return &Config{}
 }
 
 type Lowercase struct {
-	next     http.Handler
-	name     string
+	next http.Handler
+	name string
 }
 
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
 	return &Lowercase{
-		next:     next,
-		name:     name,
+		next: next,
+		name: name,
 	}, nil
 }
 
 func (a *Lowercase) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	var uppercaseChars = regexp.MustCompile(`.*[A-Z].*`)
-	if (uppercaseChars.MatchString(req.URL.Path)) {
-		http.Redirect(rw, req, strings.ToLower(req.URL.Path) , http.StatusMovedPermanently)
+	if uppercaseChars.MatchString(req.URL.Path) {
+		http.Redirect(rw, req, strings.ToLower(req.URL.Path), http.StatusMovedPermanently)
 	} else {
 		a.next.ServeHTTP(rw, req)
 	}
